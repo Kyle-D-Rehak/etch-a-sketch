2420ae2bd745622a87e383cf4997e3a3c0ff7de3
Kyle Rehak <kylerehak@gmail.com>
2022-07-16 19:34:29 +0800

    Add color and erase button

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const canvas = document.querySelector('#canvas');
+const colorButton = document.querySelector('#colorButton');
+const eraseButton = document.querySelector('#eraseButton');
 const clearButton = document.querySelector('#clearButton');
 const resetButton = document.querySelector('#resetButton');
+const colorDisplay = document.querySelector('#colorDisplay');
 const slider = document.querySelector('.slider');
 const sliderLabel = document.querySelector('#sliderLabel');
-let color = 'black';
+let color = '#2b2b2b';
 let mouseDown = false;
 
 document.addEventListener('mousedown', () => mouseDown = true);
 document.addEventListener('mouseup', () => mouseDown = false);
 
 
-slider.addEventListener('change', () => createCanvas(slider.value));
+slider.addEventListener('change', () => {createCanvas(slider.value); color = '#2b2b2b'; updateColorDisplay(color);});
 slider.addEventListener('input', () => updateSliderText(slider.value));
 
+colorButton.addEventListener('click', () => {color = '#2b2b2b'; updateColorDisplay(color);});
+eraseButton.addEventListener('click', () => {color = 'white'; updateColorDisplay(color);});
 clearButton.addEventListener('click', () => createCanvas(slider.value));
-resetButton.addEventListener('click', () => {slider.value = 16; createCanvas(slider.value); updateSliderText(slider.value)});
+resetButton.addEventListener('click', () => {slider.value = 16; createCanvas(slider.value); updateSliderText(slider.value); color = '#2b2b2b'; updateColorDisplay(color);});
 
 
 createCanvas(slider.value);
@@ -22,6 +27,10 @@ sliderLabel.textContent = `${slider.value} x ${slider.value}`;
 
 function updateSliderText(value) {
     sliderLabel.textContent = `${value} x ${value}`;
+}
+
+function updateColorDisplay(color) {
+    colorDisplay.setAttribute('style', `background-color: ${color}`);
 }
 
 function createCanvas(size) {
